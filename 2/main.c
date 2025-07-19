fc8a8f8461d9c649cabb63da26ae550c72567812
Nathan <hello@gougoule.ch>
2025-07-19 12:08:01 +0200

    wip

--- 2/main.c
+++ 2/main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,6 +9,25 @@ typedef struct BackpropValue {
     char operation;
     float grad;
 } BackpropValue;
+
+typedef struct Neuron {
+    struct BackpropValue **w;
+    int nin;
+    struct BackpropValue *b;
+} Neuron;
+
+typedef struct Layer {
+    struct Neuron **neurons;
+    int nout;
+} Layer;
+
+typedef struct NN {
+    struct Layer **layers;
+    int n_layers;
+    int nin;
+    int nout;
+    int nhin; // Number of hidden neurons in every layer of the hidden layers
+} NN;
 
 int displayValueWithDepth(BackpropValue *bv, int depth) {
     printf("%p Value: %f, Parents: %p, NumParents: %d, Operation: %c, Grad : %f\n",
@@ -58,13 +78,13 @@ int multiplyValues(BackpropValue *a, BackpropValue *b, BackpropValue *result) {
     return 0;
 }
 
-int reluValue(BackpropValue *a, BackpropValue *result) {
-    result->value = a->value > 0 ? a->value : 0;
+int tanhValue(BackpropValue *a, BackpropValue *result) {
+    result->value = tanhf(a->value);
 
-    result->parents = malloc(sizeof(BackpropValue*));
+    result->parents = malloc(sizeof(BackpropValue*) * 1);
     result->parents[0] = a;
     result->num_parents = 1;
-    result->operation = 'R';
+    result->operation = 'T'; // No operation
     return 0;
 }
 
@@ -80,17 +100,16 @@ int buildTopo(BackpropValue *bv, BackpropValue ***topo, int *index) {
             }
         }
         if (found == 0) {
+            buildTopo(bv->parents[i], topo, index);
             *topo = realloc(*topo, sizeof(BackpropValue *) * (*index + 1));
             (*topo)[*index] = (bv->parents[i]);
             (*index)++;
-            buildTopo(bv->parents[i], topo, index);
         }
     }
     return 0;
 }
 
 int _backwardValue(BackpropValue *bv) {
-    printf("Current grad: %f\n", bv->grad);
     if(bv->operation == '_') {
         return 0;
     } else if(bv->operation == '+') {
@@ -102,10 +121,9 @@ int _backwardValue(BackpropValue *bv) {
     } else if(bv->operation == '*') {
         bv->parents[0]->grad += bv->grad * bv->parents[1]->value;
         bv->parents[1]->grad += bv->grad * bv->parents[0]->value;
-    } else if(bv->operation == 'R') {
-        if(bv->parents[0]->value > 0) {
-            bv->parents[0]->grad += bv->grad;
-        }
+    } else if(bv->operation == 'T') {
+        float tanh_val = tanhf(bv->parents[0]->value);
+        bv->parents[0]->grad += bv->grad * (1 - tanh_val * tanh_val);
     } else {
         return -1; // Unknown operation
     }
@@ -126,20 +144,11 @@ int backwardValue(BackpropValue *bv) {
     BackpropValue **topo = NULL;
     int *topo_length = malloc(sizeof(int));
 
-    // TODO : IMPORTANT
-    // IMPORTANT
-    // IMPORTANT
-    // IMPORTANT
-    // IMPORTANT
-    // IMPORTANT
-    // IMPORTANT
-    // IMPORTANT
-    // YOU NEED TO BUILD THE TOPO IN BACKWARD ORDER, THEN REVERSE IT. TO HANDLE EDGE CASES (WHICH ARE NOT ""EDGE"" CASES WHEN BUUILDING MLP)
     buildTopo(bv, &topo, topo_length);
 
     bv->grad = 1.0f; // Set the gradient of the output value to 1.0
     _backwardValue(bv);
-    for(int i = 0; i < *topo_length;i++) {
+    for(int i = *topo_length-1; i >= 0; i--) {
         _backwardValue(topo[i]);
     }
 
@@ -151,31 +160,76 @@ int backwardValue(BackpropValue *bv) {
     return 0;
 }
 
+int createValue(float value, BackpropValue *bv) {
+    bv->value = value;
+    bv->parents = NULL;
+    bv->num_parents = 0;
+    bv->operation = '_'; // No operation
+    bv->grad = 0.0f; // Initialize gradient to 0
+    return 0;
+}
+
+int createNeuron(int nin, Neuron *n) {
+    n->nin = nin;
+    n->w = malloc(sizeof(BackpropValue*) * nin);
+    for(int i = 0; i < nin; i++) {
+        // random float between -1.0 and 1.0
+        float random_value = ((float)rand() / RAND_MAX) * 2.0f - 1.0f; // Random float in [-1, 1]
+        n->w[i] = malloc(sizeof(BackpropValue));
+        createValue(random_value, n->w[i]);
+    }
+    float random_bias = ((float)rand() / RAND_MAX) * 2.0f - 1.0f; // Random float in [-1, 1]
+    n->b = malloc(sizeof(BackpropValue));
+    createValue(random_bias, n->b);
+    return 0;
+}
+
+int callNeuron(Neuron *n, BackpropValue **inputs, BackpropValue *output) {
+    for(int i = 0; i < n->nin; i++) {
+        BackpropValue *weighted_input = malloc(sizeof(BackpropValue));
+        multiplyValues(n->w[i], inputs[i], weighted_input);
+    }
+}
+
+int createLayer(int nin, int nout, Layer *l) {
+    l->nout = nout;
+    l->neurons = malloc(sizeof(Neuron*) * nout);
+    for(int i = 0; i < nout; i++) {
+        l->neurons[i] = malloc(sizeof(Neuron));
+        createNeuron(nin, l->neurons[i]);
+    }
+    return 0;
+}
+
+int createNN(int nin, int nout, int n_layers, NN *nn, int nhin) {
+    if (n_layers < 2) {
+        fprintf(stderr, "Error: Number of layers must be at least 2, including input and output layers.\n");
+        return -1;
+    }
+
+    nn->nin = nin;
+    nn->nout = nout;
+    nn->n_layers = n_layers;
+    nn->layers = malloc(sizeof(Layer*) * n_layers);
+    nn->nhin = nhin;
+
+    nn->layers[0] = malloc(sizeof(Layer));
+    createLayer(nin, nhin, nn->layers[0]); // Input layer
+
+    for(int i = 1; i < n_layers - 1; i++) {
+        nn->layers[i] = malloc(sizeof(Layer));
+        createLayer(nhin, nhin, nn->layers[i]);
+    }
+
+    nn->layers[n_layers - 1] = malloc(sizeof(Layer));
+    createLayer(nhin, nout, nn->layers[n_layers - 1]); // Output layer
+
+    return 0;
+}
 
 int main() {
-    BackpropValue c;
-
-    BackpropValue a;
-    a.value = 10.0f;
-    a.parents = NULL;
-    a.num_parents = 0;
-    a.operation = '_'; 
-    a.grad = 0.0f;
-
-    BackpropValue b;
-    b.value = 32.0f;
-    b.parents = NULL;
-    b.num_parents = 0;
-    b.operation = '_';
-    b.grad = 0.0f;
-
-    addValues(&a, &b, &c);
-
-    BackpropValue test;
-    multiplyValues(&a, &c, &test);
-
-    resetGrad(&test);
-    backwardValue(&test);
+    Layer *layer = malloc(sizeof(Layer));
+    createLayer(3, 2, layer);
 
     return 0; 
 }
